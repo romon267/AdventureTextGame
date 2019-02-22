227cbecc840b7903c367ddae02e69edc6342c73a
romon267 <romon267@gmail.com>
2019-02-22 18:17:13 +0300

    Just Started it today, added character class.

--- AdventureGame/Program.cs
+++ AdventureGame/Program.cs
@@ -10,6 +10,35 @@ namespace AdventureGame
     {
         static void Main(string[] args)
         {
+            // Just trying some stuff and checking if it works as i think.
+
+            Character player = new Character("Blewboar", 100, 50);
+            Character enemy = new Character("Alkaizer", 500, 1);
+            Console.WriteLine("Player object has:\nname: {0}\nHealth: {1}\nAttack: {2}", player.Name, player.Health, player.Attack);
+            Console.WriteLine("Enemy object has:\nname: {0}\nHealth: {1}\nAttack: {2}", enemy.Name, enemy.Health, enemy.Attack);
+
+            Console.WriteLine(enemy);
+            Console.WriteLine(player);
+            
+            Console.ReadLine(); // Anti-close
+
+
         }
+    }
+
+    class Character // basic class for any entity in game, will add more properties later.
+    {
+        public string Name { get; set; }
+        public int Health { get; set; }
+        public int Attack { get; set; }
+
+        public Character(string name, int health, int attack)
+        {
+            Name = name;
+            Health = health;
+            Attack = attack;
+        }
+            
+
     }
 }
